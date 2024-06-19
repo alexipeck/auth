d66f53b45c0e0446387c6594eb62d9530dba8b29
Alexi Peck <alexinicolaspeck@gmail.com>
2024-06-19 16:22:38 +0000

    Remove custom serde implementations for get_write_token route's input data type. Switched from using query parameters to just JSON in the body due to how overcomplicated serializing [u8; 6] to query params had become, even after making the serde function use a String. Serialization worked, but whatever custom serde logic exists in reqwest .query() errored at runtime

--- src/routes/authenticated.rs
+++ src/routes/authenticated.rs
@@ -1,6 +1,6 @@
-use crate::{auth_manager::AuthManager, str_to_two_fa};
+use crate::auth_manager::AuthManager;
 use axum::{
-    extract::{ConnectInfo, Query},
+    extract::ConnectInfo,
     http::{HeaderMap, StatusCode},
     response::IntoResponse,
     Extension, Json,
@@ -9,12 +9,8 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
-use serde::{
-    de::{self, MapAccess, Visitor},
-    ser::SerializeStruct,
-};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-use std::{fmt, net::SocketAddr, sync::Arc};
+use serde::{Deserialize, Serialize};
+use std::{net::SocketAddr, sync::Arc};
 use tracing::warn;
 
 pub async fn refresh_read_token_route(
@@ -33,70 +29,9 @@ pub async fn refresh_read_token_route(
     }
 }
 
-pub struct GetWriteTokenQueryParams {
+#[derive(Serialize, Deserialize)]
+pub struct GetWriteTokenData {
     two_fa_code: [u8; 6],
-}
-
-impl Serialize for GetWriteTokenQueryParams {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let two_fa_code_str =
-            std::str::from_utf8(&self.two_fa_code).map_err(serde::ser::Error::custom)?;
-        let mut state = serializer.serialize_struct("GetWriteTokenQueryParams", 1)?;
-        state.serialize_field("two_fa_code", &two_fa_code_str)?;
-        state.end()
-    }
-}
-
-impl<'de> Deserialize<'de> for GetWriteTokenQueryParams {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct GetWriteTokenQueryParamsVisitor;
-
-        impl<'de> Visitor<'de> for GetWriteTokenQueryParamsVisitor {
-            type Value = GetWriteTokenQueryParams;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("a struct representing GetWriteTokenQueryParams with a 6-character 2FA code as a string")
-            }
-
-            fn visit_map<V>(self, mut map: V) -> Result<GetWriteTokenQueryParams, V::Error>
-            where
-                V: MapAccess<'de>,
-            {
-                let mut two_fa_code = None;
-                while let Some(key) = map.next_key::<String>()? {
-                    match key.as_ref() {
-                        "two_fa_code" => {
-                            if two_fa_code.is_some() {
-                                return Err(de::Error::duplicate_field("two_fa_code"));
-                            }
-                            let value: String = map.next_value()?;
-                            two_fa_code = Some(str_to_two_fa(&value).ok_or_else(|| {
-                                de::Error::custom("invalid two_factor_code format")
-                            })?);
-                        }
-                        _ => {
-                            let _: de::IgnoredAny = map.next_value()?;
-                        }
-                    }
-                }
-                let two_fa_code =
-                    two_fa_code.ok_or_else(|| de::Error::missing_field("two_fa_code"))?;
-                Ok(GetWriteTokenQueryParams { two_fa_code })
-            }
-        }
-
-        deserializer.deserialize_struct(
-            "GetWriteTokenQueryParams",
-            &["two_fa_code"],
-            GetWriteTokenQueryParamsVisitor,
-        )
-    }
 }
 
 pub async fn get_write_token_route(
@@ -104,10 +39,10 @@ pub async fn get_write_token_route(
     Extension(auth_manager): Extension<Arc<AuthManager>>,
     headers: HeaderMap,
     TypedHeader(authorisation): TypedHeader<Authorization<Bearer>>,
-    Query(query_params): Query<GetWriteTokenQueryParams>,
+    axum::response::Json(data): axum::response::Json<GetWriteTokenData>,
 ) -> impl IntoResponse {
     match auth_manager
-        .generate_write_token(authorisation.token(), &query_params.two_fa_code, &headers)
+        .generate_write_token(authorisation.token(), &data.two_fa_code, &headers)
         .await
     {
         Ok(token_pair) => (StatusCode::OK, Json(token_pair)).into_response(),
